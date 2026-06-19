489937a955dc85482c46de1d955ac07a429080f2
Mierjar <mierjarner@gmail.com>
2026-06-19 15:51:16 +0200

    1 cifre

--- index.js
+++ index.js
@@ -45,7 +45,7 @@
             const prediction = await model.predict(webcam.canvas);
             for (let i = 0; i < maxPredictions; i++) {
                 const classPrediction =
-                    prediction[i].className + ": " + prediction[i].probability.toFixed(2);
+                    prediction[i].className + ": " + prediction[i].probability.toFixed(1);
                 labelContainer.childNodes[i].innerHTML = classPrediction;
             }
         }
